c95b9833c55c59c1db0b1469925831d71d4c554d
Coreen Cooper <coreencooper@pursuit.org>
2021-03-22 10:41:18 -0400

    apply css styling

---
+++ index.js
@@ -0,0 +1,7 @@
+// Side scroll through the `all-characters` ul and click on a character.
+// When they select a character, the `main` should appear and information about that
+// character (name, image, status, location name) should populate in the `character-info` section.
+// When they click on a different character, this information should be replaced.
+// The page `title` should also match the name of the character selected. 
+// Use the `form`, to submit a character comment. Each comment should be a new `li` inside of `character-comments-ul`,
+// with the selected character's name (in bold) and a comment (not bold).
