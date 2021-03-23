7c6e5fc477acb402a25d88a521bce620016e0455
Coreen Cooper <coreencooper@pursuit.org>
2021-03-23 12:11:14 -0400

    display img, name and update title tag

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Side scroll through the `all-characters` ul and click on a character.
-// const main = document.querySelector("main");
-// const title = document.querySelector("title");
+const main = document.querySelector("main");
+const title = document.querySelector("title");
 const allCharacters = document.querySelector("#all-characters");
 
 const getAllCharacters = (res) => {
@@ -17,39 +17,54 @@ const getAllCharacters = (res) => {
     characterName.textContent = char.name;
   });
 };
-
-const getCharacters = async () => {
-  const res = await axios.get("https://rickandmortyapi.com/api/character");
-  try {
-    getAllCharacters(res.data);
-  }
-  catch (err) {
-    console.log("Something, something, something, something just ain't right");
-  }
-}
-getCharacters();
-
 // When they select a character, the `main` should appear
 // the character's (name, image, status, location name) should populate in the `character-info` section.
 // The page `title` should also match the name of the character selected.
 // When they click on a different character, this information should be replaced.
 
-// const selectCharacter = (e) => {
-//   main.style.display = "flex";
-//   const characterName = e.target.parentElement.textContent;
-//   const name = e.target.parentElement.lastElementChild.textContent;
-//   const characterImg = document.querySelector("#character-img");
-//   const characterStatus = document.querySelector("#character-status");
-//   const characterLocation = document.querySelector("#character-location");
-//   //   const name = e.target.parentElement.textContent;
-//   title.textContent = name;
-//   characterName.textContent = name;
-//   debugger;
-//   characterImg.src = e.target.parentElement.src;
-//   //   characterStatus.textContent =
-//   //   characterLocation.textContent =
-// };
+const selectCharacter = (e) => {
+  main.style.display = "flex";
+  const characterName = document.querySelector("#character-name");
+  const name = e.target.parentElement.lastElementChild.textContent;
+  const characterImg = document.querySelector("#character-img");
+  // const characterStatus = document.querySelector("#character-status");
+  // const characterLocation = document.querySelector("#character-location");
+  title.textContent = name;
+  characterName.textContent = name;
+  characterImg.src = e.target.parentElement.firstElementChild.src;
+  //   characterStatus.textContent =
+  //   characterLocation.textContent =
+};
 
+// Use the `form`, to submit a character comment. Each comment should be a new `li` inside of `character-comments-ul`,
+// with the selected character's name (in bold) and a comment (not bold).
+
+const getCharacters = async () => {
+  try {
+    const res = await axios.get("https://rickandmortyapi.com/api/character");
+    getAllCharacters(res.data);
+  } catch (err) {
+    console.log("Something, something, something, something just ain't right");
+  }
+};
+
+allCharacters.addEventListener("click", (e) => {
+  selectCharacter(e);
+});
+
+getCharacters();
+
+// A `main` tag that starts not on the page
+// Inside of `main` should be two sections. The first section should have the id `character-info`
+// the second should have the id `character-comments-section`.
+// Inside of `character-info` should be be an `h3` an `img` and two `p` tags.
+// Inside of `character-comments-section` should be a `form`, including a "text" `input` and a "submit"
+// `input`, that allows users to submit (not save, just add to the frontend) what they would say about
+// the character Jerry. On submission the input should clear.
+// Also insider `character-comments-section`  should be a `ul` with the id `character-comments-ul` that
+// contains the submitted comments of each character.
+
+// fetch //////////////////////////////////////////
 // const getCharacters = (e) => {
 //   // const res = axios.get("https://rickandmortyapi.com/api/character")
 //   fetch("https://rickandmortyapi.com/api/character")
@@ -70,23 +85,7 @@ getCharacters();
 //   debugger;
 // });
 
-
-// A `main` tag that starts not on the page
-// Inside of `main` should be two sections. The first section should have the id `character-info`
-// the second should have the id `character-comments-section`.
-// Inside of `character-info` should be be an `h3` an `img` and two `p` tags.
-// Inside of `character-comments-section` should be a `form`, including a "text" `input` and a "submit"
-// `input`, that allows users to submit (not save, just add to the frontend) what they would say about
-// the character Jerry. On submission the input should clear.
-// Also insider `character-comments-section`  should be a `ul` with the id `character-comments-ul` that
-// contains the submitted comments of each character.
-
-// Use the `form`, to submit a character comment. Each comment should be a new `li` inside of `character-comments-ul`,
-// with the selected character's name (in bold) and a comment (not bold).
-
 // get api of all
-
-
 
 // async /////////////////////
 // // async - allows us to assign the results of fetch to a variable
